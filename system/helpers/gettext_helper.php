bcfe015291af52e3b2557509a3cb000975247964
Kader Bouyakoub <bkader@mail.com>
2016-09-26 04:24:01 +0100

    Fixing .gitignore

---
+++ system/helpers/gettext_helper.php
@@ -0,0 +1,150 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Gettext Helper
+ * @package 	CodeIgniter\CI-Gettext
+ * @category 	Helpers
+ * @author 	Kader Bouyakoub <bkader@mail.com>
+ * @link 	http://www.bkader.com/
+ */
+
+// if ( ! function_exists('gettext'))
+// {
+// 	function gettext($msgid)
+// 	{
+// 		return _gettext($msgid);
+// 	}
+// }
+
+if ( ! function_exists('line'))
+{
+	/**
+	 * Uses either gettext() OR dgettext()
+	 * @param 	string 	$msgid 		the message to look for in our MO file
+	 * @param 	mixed 	$args 		string, integer or array or arguments
+	 * @param 	string 	$domain 	domain name if to use a different one
+	 * @return 	string         		the message to return
+	 */
+	function line($msgid, $args = NULL, $domain = NULL)
+	{
+		if ($domain !== NULL && $domain <> 'messages')
+		{
+			// If we are using php-gettext library, we use
+			// their own T_bindtextdomain() function
+			if (function_exists('_gettext'))
+				T_bindtextdomain($domain, APPPATH.'language');
+			// Otherwise, we use php built-in function
+			else
+				bindtextdomain($domain, APPPATH.'language');
+		}
+
+		// If a domain is provided and is different from the
+		// default domain used, we use dgettext or _dgettext
+		if ($domain !== NULL && $domain <> 'messages')
+		{
+			$msgstr = (function_exists('_dgettext')) ? _dgettext($domain, $msgid) : dgettext($domain, $msgid);
+		}
+
+		// If no domain is provided or is the default one, we
+		// use gettext() or _gettext() functions.
+		else
+		{
+			$msgstr = (function_exists('_gettext')) ? _gettext($msgid) : gettext($msgid);
+		}
+
+		return ($args) ? vsprintf($msgstr, (array) $args) : $msgstr;
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('nline'))
+{
+	/**
+	 * Use the plural form of gettext
+	 * @param 	string 	$singular 	the singular form of the message
+	 * @param 	string 	$plural 	the plural form of the message
+	 * @param 	integer $number 	the integer used for comparison
+	 * @oaram 	string 	$domain 	in case of a different domain
+	 */
+	function nline($singular, $plural, $number, $domain = NULL)
+	{
+		// If the domain is set and different from the default one
+		if ($domain !== NULL AND $domain <> 'messages')
+		{
+			if (function_exists('_dngettext'))
+			{
+				T_bindtextdomain($domain, APPPATH.'languages');
+				return _dngettext($domain, $singular, $plural, $number);
+			}
+			else
+			{
+				bindtextdomain($domain, APPPATH.'languages');
+				return dngettext($domain, $singular, $plural, $number);
+			}
+		}
+
+		return (function_exists('_ngettext')) 
+				? _ngettext($singular, $plural, $number) 
+				: ngettext($singular, $plural, $number);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('__'))
+{
+	/**
+	 * If php_getttext extension is enabled, this function will be declared
+	 * and usable as alias of line() function. But if php_gettext is not
+	 * available, in this case we use php-gettext library that comes with
+	 * an already-declared __() function.
+	 *
+	 * PHP-Gettext's library __() function takes only one arguments which
+	 * is the $msgid while this one here takes three.
+	 *
+	 * @param 	string 	$msgid 	the message to look for
+	 * @param 	mixed 	$args 	string, number or array
+	 * @param 	string 	$domain domain name if different
+	 */
+
+	function __($msgid, $args = NULL, $domain = NULL)
+	{
+		return line($msgid, $args, $domain);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('e_line'))
+{
+	/**
+	 * This function calls line() and echoes the message
+	 * @param 	string 	$msgid 	the message to look for
+	 * @param 	mixed 	$args 	string, integer or array
+	 * @param 	string 	$domain in case of a different domain
+	 */
+	function e_line($msgid, $args = NULL, $domain = NULL)
+	{
+		echo line($msgid, $args, $domain);
+	}
+}
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('e_nline'))
+{
+	/**
+	 * This function calls nline() and echoes the message
+	 * @param 	string 	$msgid 	the message to look for
+	 * @param 	mixed 	$args 	string, integer or array
+	 * @param 	string 	$domain in case of a different domain
+	 */
+	function e_nline($singular, $plural, $number, $domain = NULL)
+	{
+		echo nline($singular, $plural, $number, $domain);
+	}
+}
+
+/* End of file gettext_helper.php */
+/* Location: ./system/helpers/gettext_helper.php */
---
+++ system/libraries/Gettext.php
@@ -0,0 +1,351 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CI_Gettext
+{
+	/**
+	 * @var object - instance of CI object
+	 */
+	protected $CI;
+
+	/**
+	 * @var array - site default language
+	 */
+	protected $default;
+
+	/**
+	 * @var array - site available languages
+	 */
+	protected $languages = array();
+
+	/**
+	 * @var string - language cookie name
+	 * @var string - language session name
+	 */
+	protected $cookie  = NULL;
+	protected $session = NULL;
+
+	/**
+	 * @var array - client supported language
+	 */
+	protected $client;
+
+	/**
+	 * @var array - site current language
+	 */
+	protected $current;
+
+	/**
+	 * @var string - gettext domain
+	 */
+	protected $domain = 'messages';
+
+	/**
+	 * Constructor
+	 */
+	public function __construct($config = array())
+	{
+		// Prepare CI object
+		$this->CI =& get_instance();
+
+		// initialize class
+		$this->initialize($config);
+	}
+
+	/**
+	 * Initialize Gettext library class
+	 * @access 	protected
+	 * @param 	array
+	 * @return 	void
+	 */
+	protected function initialize(array $config = array())
+	{
+		// Make sure the configuration is never empty
+		if (empty($config))
+		{
+			$config = array(
+				'gettext_enabled'     => TRUE,
+				'gettext_default'     => 'english',
+				'gettext_domain'      => 'messages',
+				'gettext_languages'   => array('english'),
+				'available_languages' => array(
+					'english' => array(
+						'name'    => 'English',
+						'name_en' => 'English',
+						'folder'  => 'english',
+						'code'    => 'en',
+						'flag'    => 'us',
+					),
+				),
+			);
+		}
+
+		// If gettext_enabled is set to FALSE, we return;
+		if ( ! $config['gettext_enabled']) return;
+
+		// We check whether the cookie use is enabled or not
+		isset($config['gettext_cookie']) OR $config['gettext_cookie'] = 'lang';
+		if ($config['gettext_cookie'] !== NULL)
+		{
+			function_exists('get_cookie') OR $this->CI->load->helper('cookie');
+			$this->cookie = $config['gettext_cookie'];
+		}
+
+		// If we are using session as well, we make sure session library
+		// is loaded.
+		isset($config['gettext_session']) OR $config['gettext_session'] = NULL;
+		if ($config['gettext_session'] !== NULL)
+		{
+			class_exists('CI_Session') OR $this->CI->load->library('session');
+			$this->session = $config['gettext_session'];
+		}
+
+		// Make sure available language is never empty
+		if ( ! isset($config['gettext_languages']) OR empty($config['gettext_languages']))
+		{
+			$config['gettext_languages'] = array('english');
+		}
+
+		foreach ($config['gettext_languages'] as $lang)
+		{
+			if (array_key_exists($lang, $config['available_languages']))
+			{
+				$this->languages[$lang] = $config['available_languages'][$lang];
+			}
+		}
+
+		// Now we set site default language
+		isset($config['gettext_default']) OR $config['gettext_default'] = config_item('language');
+		$this->default = $this->languages[$config['gettext_default']];
+
+		// Set our client language now
+		$this->client = $this->_set_client_language();
+
+		// Set current language
+		$this->current = $this->_set_current_language();
+
+		// Set gettext domain name
+		$config['gettext_domain'] && $this->domain = $config['gettext_domain'];
+
+		// If php_gettext extension is enabled, we use the built-in
+		// functions, otherwise, we use php-gettext library.
+        if (function_exists('__'))
+        {
+            T_setlocale(LC_MESSAGES, $this->current['folder']);
+            T_bindtextdomain($this->domain, APPPATH.'language');
+            T_bind_textdomain_codeset($this->domain, 'UTF-8');
+            T_textdomain($this->domain);
+        }
+        else
+        {
+        	putenv('LANG='.$this->current['folder']);
+        	setlocale(LC_MESSAGES, $this->current['folder']);
+        	bindtextdomain($this->domain, APPPATH."language");
+            bind_textdomain_codeset($this->domain, 'UTF-8');
+            textdomain($this->domain);
+        }
+
+        // Load gettext helper
+        $this->CI->load->helper('gettext');
+
+        log_message('debug', 'Gettext Class Initialized');
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Returns a list of available languages
+	 * @access 	public
+	 * @param 	string 	$lang 	language folder name
+	 * @param 	string 	$key 	key to return
+	 * @return 	mixed
+	 */
+	public function languages($lang = NULL, $key = NULL)
+	{
+		$return = $this->languages;
+		if ($lang !== NULL && array_key_exists($lang, $return))
+		{
+			$return = $return[$lang];
+
+			if ($key !== NULL && array_key_exists($key, $return))
+				$return = $return[$key];
+		}
+
+		return $return;
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Returns site default language default
+	 * @access 	public
+	 * @param 	string 	$key 	key to return
+	 * @return 	string
+	 */
+	public function get_default($key = NULL)
+	{
+		$return = $this->default;
+
+		if ($key !== NULL && array_key_exists($key, $return))
+			$return = $return[$key];
+
+		return $return;
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Automatically sets client's language
+	 * @access 	protected
+	 * @param 	none
+	 * @return 	array
+	 */
+	protected function _set_client_language()
+	{
+		$code = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+		$lang = $this->find_by('code', $code);
+		$lang OR $lang = $this->default;
+		return $lang;
+	}
+
+	/**
+	 * Returns client's language details
+	 * @access 	public
+	 * @param 	string 	$key 	key to return
+	 * @return 	string
+	 */
+	public function get_client($key = NULL)
+	{
+		$return = $this->client;
+		
+		if ($key !== NULL && array_key_exists($key, $return))
+			$return = $return[$key];
+
+		return $return;
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Sets site current language
+	 * @access 	protected
+	 * @param 	none
+	 * @return 	array
+	 */
+	protected function _set_current_language()
+	{
+		$_lang = NULL;
+
+		// If the use of COOKIES is enabled, we check the cookie
+		if ($this->cookie !== NULL)
+		{
+			$_lang = get_cookie($this->cookie, TRUE);
+			$_lang OR $_lang = $this->get_client('folder');
+		}
+
+		// In case we use SESSION instead of COOKIE
+		elseif($this->session !== NULL)
+		{
+			$_lang = $this->CI->session->{$this->session};
+			$_lang OR $_lang = $this->get_client('folder');
+		}
+
+		// If neither COOKIE nor SESSION are used, we use
+		// default language
+		else
+		{
+			$_lang = $this->default['folder'];
+		}
+
+		// We prepare our default language
+		$current = $this->default;
+
+		// We make sure the language is available
+		if ($lang = $this->find_by('folder', $_lang))
+		{
+			$current = $lang;
+			unset($lang);
+		}
+
+		// We set COOKIE if enabled
+		if ($this->cookie !== NULL)
+			set_cookie($this->cookie, $current['folder'], 2678400);
+
+		// If no cookie but session is ON
+		elseif($this->session !== NULL)
+			$_SESSION[$this->session] = $current['folder'];
+
+		return $current;
+	}
+
+	/**
+	 * Returns current language's details
+	 * @access 	public
+	 * @param 	string 	$key 	key to return
+	 * @return 	string
+	 */
+	public function get_current($key = NULL)
+	{
+		$return = $this->current;
+
+		if ($key !== NULL && array_key_exists($key, $return))
+			$return = $return[$key];
+
+		return $return;
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Search through languages array
+	 * @access 	public
+	 * @param 	string 	$field 	field to compare to
+	 * @param 	string 	$match 	value to compare field to
+	 * @return 	array
+	 */
+	public function find_by($field = 'folder', $match = 'english')
+	{
+		foreach($this->languages as $lang)
+		{
+			if (isset($lang[$field]) && $lang[$field] === $match)
+				return $lang;
+		}
+
+		return NULL;
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Change current language
+	 * @access 	public
+	 * @param 	string 	$code 	language code to use
+	 * @return 	boolean
+	 */
+	public function change($code = 'en')
+	{
+		// We make sure the language exists
+		if ($lang = $this->find_by('code', $code))
+		{
+			// If the use of cookies is ON
+			if ($this->cookie !== NULL)
+				set_cookie($this->cookie, $lang['folder']);
+
+			// In case COOKIE are off but SESSION is on
+			elseif ($this->session !== NULL)
+				$_SESSION[$this->session] = $lang['folder'];
+
+			// If neither cookie nor session are on
+			// we simply change the current language.
+			else
+				$this->current = $lang;
+
+			return TRUE;
+		}
+
+		return FALSE;
+	}
+}
+
+/* End of file Gettext.php */
+/* Location: ./system/libraries/Gettext.php */
